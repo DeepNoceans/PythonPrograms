3e04290941e39dac260f47dba2bbeb5ebbd356f9
DeepNoceans <80008534+DeepNoceans@users.noreply.github.com>
2021-03-12 15:51:06 -0600

    3/12
    2021

--- SimpleGUI/00test.py
+++ SimpleGUI/00test.py
@@ -2,13 +2,15 @@ from tkinter import *
 
 root = Tk()
 root.title("Go!")
-root.geometry("500x500")
-root.mainloop()
+root.geometry("200x100")
+
 
 #create a frame (holds other widgets, in window)
-app = Frame (root)
+app = Frame(root)
 app.grid()
 
 #create button (in frame)
-button1 = Button(app)
+button1 = Button(app, text = "Go!")
 button1.grid()
+button1.configure(text = "Warning")
+root.mainloop()
