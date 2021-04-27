48e12fabff025a0c697e96d779d225766f8a7dac
DeepNoceans <80008534+DeepNoceans@users.noreply.github.com>
2021-04-27 13:54:30 -0500

    4/27
    Finished Bitmap (saved via Jaden) and started Escape the Dragon

--- Pygame!/L5/L5_BitMap.py
+++ Pygame!/L5/L5_BitMap.py
@@ -1,38 +1,19 @@
-import sys, random, math, pygame
+import sys
+import random
+import math
+import pygame
 from pygame.locals import *
 
-def print_text(font, x, y, text, color=(255,255,255)):
-    imgText = font.render(text, True, color)
-    screen.blit(imgText, (x,y))
-
-def wrap_angle(angle):
-    return angle % 360
-
-# main program begins
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Orbit Demo")
-font = pygame.font.Font(None, 18)
-
-
-#load bitmap
-space = pygame.image.load("space.png").convert_alpha()
-
-#load bitmap
-planet = pygame.image.load("planet2.png").convert_alpha()
-width, height = planet.get_size()
-
-ship = pygame.image.load("freelance.png").convert_alpha()
-ship = pygame.transform.scale(ship, (width//2, height//2))
-#repeating loop
 
 class Point(object):
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
     # X property
+
     def getx(self):
         return self.__x
+
     def setx(self, x):
         self.__x = x
     x = property(getx, setx)
@@ -40,6 +21,7 @@ class Point(object):
     # Y property
     def gety(self):
         return self.__y
+
     def sety(self, y):
         self.__y = y
     y = property(gety, sety)
@@ -48,15 +30,50 @@ class Point(object):
         return "{X:" + "{:.0f}".format(self.__x) + \
             ",Y:" + "{:.0f}".format(self.__y) + "}"
 
+
+def wrap_angle(angle):
+    return angle % 360
+
+
+def print_text(font, x, y, text, color=(255, 255, 255)):
+    imgText = font.render(text, True, color)
+    screen.blit(imgText, (x, y))
+
+
+# main program begins
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Orbit Demo")
+font = pygame.font.Font(None, 38)
+
+
+#load bitmap
+space = pygame.image.load("space.png").convert_alpha()
+
+#load bitmap
+planet = pygame.image.load("planet2.png").convert_alpha()
+width_p, height_p = planet.get_size()
+
+ship = pygame.image.load("freelance.png").convert_alpha()
+width, height = ship.get_size()
+ship = pygame.transform.scale(ship, (width//3, height//3))
+#repeating loop
+
+
 radius = 250
 angle = 0.0
-pos = Point(0,0)
-old_pos = Point(0,0)
-
-
-angle = wrap_angle(angle - 0.1)
+pos = Point(0, 0)
+old_pos = Point(0, 0)
 pos.x = math.sin(math.radians(angle)) * radius
 pos.y = math.cos(math.radians(angle)) * radius
+
+cir = math.pi * radius * 2
+cir = "{:.002f}".format(cir)
+vel = .01
+
+white = 255, 255, 255
+# myfont = pygame.font.Font(None, 20)
+textImage = font.render(f"Orbiral Circumference: {cir}", True, white)
 
 width, height = ship.get_size()
 screen.blit(ship, (400 + pos.x-width // 2, 300 + pos.y-height // 2))
@@ -68,7 +85,15 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[K_ESCAPE]:
         sys.exit()
-    
+    if keys[K_EQUALS]:
+        vel += .001
+    if keys[K_MINUS]:
+        vel -= .001
+        if vel <= 0:
+            vel = 0
+
+    angle = wrap_angle(angle - vel)
+
     delta_x = (pos.x - old_pos.x)
     delta_y = (pos.y - old_pos.y)
     rangle = math.atan2(delta_y, delta_x)
@@ -81,16 +106,18 @@ while True:
     angle = wrap_angle(angle - 0.1)
     pos.x = math.sin(math.radians(angle)) * radius
     pos.y = math.cos(math.radians(angle)) * radius
-    
-    #draw background
-    screen.blit(space, (0,0))
 
-    screen.blit(planet, (350-width/2,250-height/2))
+    #draw background
+    screen.blit(space, (0, 0))
+
+    screen.blit(textImage, (0, 0))
+
+    screen.blit(planet, (400-width_p/2, 300-height_p/2))
 
     width, height = scratch_ship.get_size()
     x = 400+pos.x-width//2
     y = 300+pos.y-height//2
-    
-    screen.blit(scratch_ship, (x,y))
+
+    screen.blit(scratch_ship, (x, y))
 
     pygame.display.update()
